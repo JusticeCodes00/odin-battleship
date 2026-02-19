336f58f7ab0d5614a3c68cc3e163240e08858eb0
justiceCodes <171641141+JusticeCodes00@users.noreply.github.com>
2026-02-19 05:22:07 +0100

    test: added tests to check board main functionalities

--- src/__tests__/Gameboard.test.js
+++ src/__tests__/Gameboard.test.js
@@ -1,7 +1,7 @@
 import { Gameboard } from "../models/Gameboard.js";
 
 describe("Gameboard", () => {
-  const board = new Gameboard();
+  let board = new Gameboard();
 
   const cols = {
     A: 0,
@@ -29,45 +29,124 @@ describe("Gameboard", () => {
     10: 9,
   };
 
+  beforeEach(() => {
+    board = new Gameboard();
+  });
+
   it("should place ships at specific coordinates", () => {
-    const ship = {
+    let ship = {
       name: "carrier",
       direction: "row",
       x: rows[1],
-      y: cols[A],
+      y: cols["A"],
     };
 
     board.placeShip(ship);
 
-    expect(board.getBoard()[1][A].name).toBe("carrier");
-    expect(board.getBoard()[1][B].name).toBe("carrier");
-    expect(board.getBoard()[1][C].name).toBe("carrier");
-    expect(board.getBoard()[1][D].name).toBe("carrier");
-    expect(board.getBoard()[1][E].name).toBe("carrier");
+    expect(board.getBoard()[1][cols["A"]].name).toBe("carrier");
+    expect(board.getBoard()[1][cols["B"]].name).toBe("carrier");
+    expect(board.getBoard()[1][cols["C"]].name).toBe("carrier");
+    expect(board.getBoard()[1][cols["D"]].name).toBe("carrier");
+    expect(board.getBoard()[1][cols["E"]].name).toBe("carrier");
 
     ship = {
       name: "destroyer",
-      direction: "column",
-      y: cols[A],
+      direction: "col",
+      y: cols["A"],
       x: rows[2],
     };
 
     board.placeShip(ship);
 
-    expect(board.getBoard()[A][2].name).toBe("destroyer");
-    expect(board.getBoard()[A][3].name).toBe("destroyer");
+    expect(board.getBoard()[2][cols["A"]].name).toBe("destroyer");
+    expect(board.getBoard()[3][cols["A"]].name).toBe("destroyer");
   });
 
-  const coordinate = { x: rows[1], y: cols[A] };
-
   it("should receive a hit", () => {
-    expect(board.getBoard()[coordinate.x][coordinate.y].getHits).toBe(1);
+    const attack = { x: rows[1], y: cols["A"] };
+
+    const ship = {
+      name: "destroyer",
+      direction: "col",
+      x: attack.x,
+      y: attack.y,
+    };
+
+    board.placeShip(ship);
+
+    expect(board.receiveAttack(attack)).toBe("hit");
+
+    expect(board.getBoard()[attack.x][attack.y].getHits()).toContainEqual([
+      attack.x,
+      attack.y,
+    ]);
   });
 
   it("should record a miss", () => {
-    expect(board.getMissedAttacks()).toContainEqual([
-      coordinate.x,
-      coordinate.y,
-    ]);
+    const attack = { x: rows[1], y: cols["A"] };
+
+    expect(board.receiveAttack(attack)).toBe("miss");
+
+    expect(board.getMissedAttacks()).toContainEqual([attack.x, attack.y]);
+  });
+
+  it("should be able to report whether or not all of their ships have been sunk.", () => {
+    const destroyer = {
+      name: "destroyer",
+      direction: "col",
+      len: 2,
+      x: rows[9],
+      y: cols["A"],
+    };
+
+    const battleship = {
+      name: "battleship",
+      direction: "col",
+      len: 4,
+      x: rows[1],
+      y: cols["J"],
+    };
+
+    const carrier = {
+      name: "carrier",
+      direction: "col",
+      len: 5,
+      x: rows[1],
+      y: cols["A"],
+    };
+
+    const submarine = {
+      name: "submarine",
+      direction: "col",
+      len: 3,
+      x: rows[8],
+      y: cols["J"],
+    };
+
+    const cruiser = {
+      name: "cruiser",
+      direction: "row",
+      len: 3,
+      x: rows[1],
+      y: cols["B"],
+    };
+
+    const ships = [carrier, battleship, submarine, destroyer, cruiser];
+
+    ships.forEach((ship) => {
+      board.placeShip(ship);
+    });
+
+    ships.forEach((ship) => {
+      for (let i = 0; i < ship.len; i++) {
+        if (ship.direction === "col") {
+          board.receiveAttack(ship.x + i, ship.y);
+        } else if (ship.direction === "row") {
+          board.receiveAttack(ship.x, ship.y + i);
+        }
+      }
+    });
+
+    expect(board.allSunk()).toBe(true);
   });
 });
